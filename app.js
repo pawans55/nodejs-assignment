d287b0b6827be137faa4284f13beb52f574c8e00
Pawan Shrivastava <pawanshrivastav555@gmail.com>
2018-06-24 17:20:53 -0700

    Update app.js

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const app = express()
 const config = require('./config.json')
 
 app.get('/', function (req, res) {
-  res.send(config.message||'Shrivastava!')
+  res.send(config.message||'Shrivastava pawan welcome')
 })
 
 app.listen(config.port, function () {
